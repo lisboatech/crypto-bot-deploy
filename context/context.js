ff91f3507efe7e3dac96aa5de6d6705b959cea05
lisboatech <lisboatech@proton.me>
2024-06-04 19:08:49 -0300

    update

--- context/context.js
+++ context/context.js
@@ -71,7 +71,7 @@ export const PROVIDER = ({children}) => {
     const quoterAddress = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6";
     
     const ROUTER = (PROVIDER) => {
-        const router = new ethers.Contract(
+        const router = new ethers.utils.Contract(
           routerAddress,
           [
             "function exactInputSingle((address tokenIn, address tokenOut, uint24 fee, address recipient, uint256 deadline, uint256 amountIn, uint256 amountOutMinimum, uint160 sqrtPriceLimitX96)) external payable returns (uint256 amountOut)",
@@ -82,7 +82,7 @@ export const PROVIDER = ({children}) => {
     };
     
     const QUOTER = (PROVIDER) => {
-        const quoter = new ethers.Contract(
+        const quoter = new ethers.utils.Contract(
           quoterAddress,
           [
             "function quoteExactInputSingle(address tokenIn, address tokenOut, uint24 fee, uint256 amountIn, uint160 sqrtPriceLimitX96) public view returns (uint256 amountOut)",
@@ -93,7 +93,7 @@ export const PROVIDER = ({children}) => {
     };
 
     const TOKEN = (PROVIDER, TOKEN_B) => {
-        const token = new ethers.Contract(
+        const token = new ethers.utils.Contract(
           TOKEN_B,
           [
             "function approve(address spender, uint256 amount) external returns (bool)",
